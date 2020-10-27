08d021a9d6a4ebdff75bc8be7fa117763b6ce381
Haripriya <haripriya@qburst.com>
2020-10-27 21:24:12 +0530

    Basic form validations for add new project form

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,12 +1,35 @@
-let modal = document.getElementById("myModal");
+// Add new project 
 const newProject = document.getElementById("new-project")
-
-newProject.addEventListener('click', event => {
+newProject.addEventListener('click', (event) => {
     modal.style.display = "flex"
 })
 
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+// Add new project form
+const modal = document.getElementById("myModal");
+const span = document.getElementById("close");
+span.onclick = () => modal.style.display = "none";
+
+// Add new project form validation
+const addProject = document.getElementById("add-project")
+addProject.addEventListener('click', (event) => console.log(event))
+
+// Date validation
+const startDate = document.getElementById("start-date"),
+endDate = document.getElementById("end-date")
+
+
+const formatDate = (date) => {
+    let d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2)  day = '0' + day;
+
+    return [year, month, day].join('-');
 }
+
+startDate.setAttribute("min", formatDate(new Date()))
+endDate.setAttribute("min", formatDate(new Date()))
+
